2fe749c60707104e6c7ec2224ce673a81ba4eb74
Wheados <Wheadosz@hotmail.com>
2022-02-13 17:19:23 -0300

    store songs on localStorage

--- ativ-03/script.js
+++ ativ-03/script.js
@@ -20,14 +20,27 @@ var musicaDiv = document.getElementById("letramusicaDiv");
 function adicionarLetraDiv(letra) {
   letraJson = JSON.parse(letra);
   musicaDiv.innerHTML = "<pre>" + letraJson.lyrics + "</pre>";
+
+  let key = artista.toLowerCase();
+  let value = musica.toLowerCase();
+  if(localStorage.getItem(key) === null){
+     localStorage.setItem(key,value);
+  } else {
+     let musicas = localStorage.getItem(key);
+     if(!musicas.includes(value)){
+        localStorage.setItem(key, musicas + " | " + value);
+     }
+
+  }
 }
 
 const artistaInput = document.getElementById("artistaInput");
 const musicaInput = document.getElementById("musicaInput");
 
 function pesquisarLetra(){
- artista = artistaInput.value;
- musica = musicaInput.value;
- xhttpAssincrono(adicionarLetraDiv);
+  artista = artistaInput.value;
+  musica = musicaInput.value;
+  if(artista == "" || musica == "") { return;}
+  xhttpAssincrono(adicionarLetraDiv);
 
 }
