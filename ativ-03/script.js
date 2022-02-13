c5b6cd9aa2c2bda576401aa337578f77d219337c
Wheados <Wheadosz@hotmail.com>
2022-02-13 17:41:00 -0300

    add musics to localStorage, recover them

--- ativ-03/script.js
+++ ativ-03/script.js
@@ -16,11 +16,34 @@ function xhttpAssincrono(callBackFunction) {
   xhttp.send();
 }
 
+//Carrega músicas do localStorage e adicionam na Div
+var musicasCacheDiv = document.getElementById("musicasCache");
+function carregarMusicasStorage(){
+  musicasCacheDiv.innerHTML = "";
+  Object.keys(localStorage).forEach(key => {
+        let musicasNome = localStorage.getItem(key);
+        let musicasArtista = musicasNome.split("|").forEach(musicaCache =>{
+           musicasCacheDiv.innerHTML += "<li>" + key + " - " + musicaCache + "</li>";
+        });
+
+
+  });
+
+}
+
+//Chama ao iniciar site
+carregarMusicasStorage();
+
+//Adiciona letra da música na div
 var musicaDiv = document.getElementById("letramusicaDiv");
 function adicionarLetraDiv(letra) {
   letraJson = JSON.parse(letra);
   musicaDiv.innerHTML = "<pre>" + letraJson.lyrics + "</pre>";
 
+}
+
+//Adiciona música no localStorage, caso artista (key) já exista, adiciona música no value
+function adicionarMusicaStorage(){
   let key = artista.toLowerCase();
   let value = musica.toLowerCase();
   if(localStorage.getItem(key) === null){
@@ -37,10 +60,12 @@ function adicionarLetraDiv(letra) {
 const artistaInput = document.getElementById("artistaInput");
 const musicaInput = document.getElementById("musicaInput");
 
+//Pesquisa a letra da música ao apertar botão
 function pesquisarLetra(){
   artista = artistaInput.value;
   musica = musicaInput.value;
   if(artista == "" || musica == "") { return;}
   xhttpAssincrono(adicionarLetraDiv);
-
+  adicionarMusicaStorage();
+  carregarMusicasStorage();
 }
